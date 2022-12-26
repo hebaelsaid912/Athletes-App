dece931b91572637ef30cd99d3d94dc967c02cba
Heba Elsaid <hebaelsaid912@gmail.com>
2022-12-26 18:01:47 +0200

    handle athletes list view model

---
+++ app/src/main/java/com/hebaelsaid/android/athletesapp/ui/feature/homelist/HomeListViewModel.kt
@@ -0,0 +1,50 @@
+package com.hebaelsaid.android.athletesapp.ui.feature.homelist
+
+import android.util.Log
+import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
+import com.hebaelsaid.android.athletesapp.data.model.AthletesListResponseModel
+import com.hebaelsaid.android.athletesapp.domain.usecase.HomeListUseCase
+import com.hebaelsaid.android.athletesapp.utils.Resource
+import dagger.hilt.android.lifecycle.HiltViewModel
+import kotlinx.coroutines.flow.MutableStateFlow
+import kotlinx.coroutines.flow.StateFlow
+import kotlinx.coroutines.flow.launchIn
+import kotlinx.coroutines.flow.onEach
+import javax.inject.Inject
+
+private const val TAG = "HomeListViewModel"
+@HiltViewModel
+class HomeListViewModel @Inject constructor(
+    private val useCase: HomeListUseCase
+) : ViewModel(){
+    private val _athletesListState = MutableStateFlow<AthletesListState>(AthletesListState.Idle)
+    val athletesListState: StateFlow<AthletesListState> get() = _athletesListState
+
+    sealed class AthletesListState {
+        data class Success(var athletesListResponseModel: AthletesListResponseModel) : AthletesListState()
+        data class Error(val message: String) : AthletesListState()
+        class Loading(val isLoading: Boolean) : AthletesListState()
+        object Idle : AthletesListState()
+    }
+    init {
+        getAthletesList()
+    }
+    private fun getAthletesList() {
+        useCase().onEach { resultState ->
+            when (resultState) {
+                is Resource.Success -> {
+                    _athletesListState.value = AthletesListState.Success(athletesListResponseModel = resultState.data!!)
+                }
+                is Resource.Loading -> {
+                    Log.d(TAG, "getAthletesList: Resource.Loading: true")
+                    _athletesListState.value = AthletesListState.Loading(isLoading = true)
+                }
+                is Resource.Error -> {
+                    Log.d(TAG, "getAthletesList: Resource.Error")
+                    _athletesListState.value = AthletesListState.Error(message = resultState.message ?: "un expected error occurred")
+                }
+            }
+        }.launchIn(viewModelScope)
+    }
+}
