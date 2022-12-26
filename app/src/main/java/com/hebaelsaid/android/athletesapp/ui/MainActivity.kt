166af514b5cf9173a55fcffebfbdbea5759ae8d6
Heba Elsaid <hebaelsaid912@gmail.com>
2022-12-26 17:43:14 +0200

    add dagger hit dependency injection

--- app/src/main/java/com/hebaelsaid/android/athletesapp/ui/MainActivity.kt
+++ app/src/main/java/com/hebaelsaid/android/athletesapp/ui/MainActivity.kt
@@ -2,11 +2,16 @@ package com.hebaelsaid.android.athletesapp.ui
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.databinding.DataBindingUtil
 import com.hebaelsaid.android.athletesapp.R
+import com.hebaelsaid.android.athletesapp.databinding.ActivityMainBinding
+import dagger.hilt.android.AndroidEntryPoint
 
+@AndroidEntryPoint
 class MainActivity : AppCompatActivity() {
+    private lateinit var binding: ActivityMainBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = DataBindingUtil.setContentView(this,R.layout.activity_main)
     }
 }
---
+++ app/src/main/java/com/hebaelsaid/android/athletesapp/ui/di/AthletesApp.kt
@@ -0,0 +1,7 @@
+package com.hebaelsaid.android.athletesapp.ui.di
+
+import android.app.Application
+import dagger.hilt.android.HiltAndroidApp
+
+@HiltAndroidApp
+class AthletesApp: Application()
