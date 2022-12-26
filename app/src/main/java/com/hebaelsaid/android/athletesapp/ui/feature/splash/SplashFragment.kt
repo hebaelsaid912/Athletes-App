6ea9c90baf46c27d7b470eb94d3cbc19a82cbe3b
Heba Elsaid <hebaelsaid912@gmail.com>
2022-12-26 18:44:37 +0200

    handle splash screen design and logic

--- app/src/main/java/com/hebaelsaid/android/athletesapp/ui/feature/splash/SplashFragment.kt
+++ app/src/main/java/com/hebaelsaid/android/athletesapp/ui/feature/splash/SplashFragment.kt
@@ -1,10 +1,13 @@
 package com.hebaelsaid.android.athletesapp.ui.feature.splash
 
 import android.os.Bundle
+import android.os.Handler
+import android.os.Looper
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
+import androidx.navigation.fragment.findNavController
 import com.hebaelsaid.android.athletesapp.databinding.FragmentSplashBinding
 
 
@@ -16,6 +19,13 @@ class SplashFragment : Fragment() {
     ): View {
         binding = FragmentSplashBinding.inflate(inflater, container, false)
         return binding.root
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        Handler(Looper.getMainLooper()).postDelayed({
+            findNavController().navigate(SplashFragmentDirections.actionSplashFragmentToHomeListFragment())
+        }, 3000)
     }
 
 }
